fa180d05ed5c955276ea5b663d38be90bb003ece
Artem Pravlotskiy <fpm.pravlock@bsu.by>
2024-03-12 20:05:44 +0300

    write logic of classic factory

--- ClassicFactory.h
+++ ClassicFactory.h
@@ -1,16 +1,50 @@
 //
 // Created by pravl on 12.03.2024.
 //
-
 #ifndef CLASSICFACTORY_H
 #define CLASSICFACTORY_H
 
 
+#include "Animal.h"
+#include "Animals/Cat.h"
+#include "Animals/Dog.h"
+#include "Animals/Fish.h"
+#include "Animals/Parrot.h"
 
-class ClassicFactory {
 
+class AnimalFactory {
+  public:
+    [[nodiscard]] virtual Animal* createAnimal() const = 0;
+    virtual ~AnimalFactory() = default;
 };
 
+class CatFactory final : public AnimalFactory {
+  public:
+    [[nodiscard]] Animal* createAnimal() const override {
+        return new Cat;
+    }
+};
+
+class DogFactory final : public AnimalFactory {
+public:
+    [[nodiscard]] Animal* createAnimal() const override {
+        return new Dog;
+    }
+};
+
+class FishFactory final : public AnimalFactory {
+public:
+    [[nodiscard]] Animal* createAnimal() const override {
+        return new Fish;
+    }
+};
+
+class ParrotFactory final : public AnimalFactory {
+public:
+    [[nodiscard]] Animal* createAnimal() const override {
+        return new Parrot;
+    }
+};
 
 
 #endif //CLASSICFACTORY_H
